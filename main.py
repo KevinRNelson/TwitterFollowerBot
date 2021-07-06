3f674dff676b36e0e0302f966b0e188b3b36f408
Kevin <keronels@ucsc.edu>
2021-07-06 11:34:17 -0700

    main no longer knows about UserFileReader -> delegated to TwitterApplication

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from twitter_application import NewFollowerApplicationBuilder
-from twitter_bot import NewFollowerTwitterBotBuilder
-
+from notification import TimedNotification, Email, Print
 from database import PickleDatabase
-from notification import *
-from file_reader import UserFileReader, ConfigFileReader
 
 EMAIL = 'keronels@ucsc.edu'
-# FILE = 'users_test.txt'
+#USER_FILE = 'users_test.txt'
 
 #EMAIL = 'luckyguy1@gmail.com'
-FILE = 'users.txt'
+USER_FILE = 'users.txt'
 
 DATABASE = 'data.pkl'
 CONFIG_FILE = 'config.txt'
@@ -17,9 +14,9 @@ CONFIG_FILE = 'config.txt'
 if __name__ == '__main__':
     application = NewFollowerApplicationBuilder()                              \
         .config_file(CONFIG_FILE)                                              \
+        .userFile(USER_FILE)                                                   \
         .notificationObject(TimedNotification(Print(EMAIL)))                   \
         .databaseObject(PickleDatabase(DATABASE))                              \
-        .fileReaderObject(UserFileReader(FILE))                                \
         .build()
 
     application.setUp()
