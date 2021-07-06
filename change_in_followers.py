b20b7c26ee2f377ae2eef1e6a87e538319e4fe52
Kevin <keronels@ucsc.edu>
2021-07-06 14:45:25 -0700

    change function declaration: change_in_followers no longer knows about database

--- change_in_followers.py
+++ change_in_followers.py
@@ -1,21 +1,19 @@
-from database import *
-
 class ChangeInFollowers():
 
     def __init__(self,
-        database: Database,
+        saved_follower_map: dict,
         follower_map: dict
     ):
 
         accounts = follower_map.keys()
 
         self.__followed_accounts = {}
-        self.initializeFollowedAccountsDict(accounts)
-        self.followedAccounts(database, follower_map)
+        self.__initializeFollowedAccountsDict(accounts)
+        self.__followedAccounts(saved_follower_map, follower_map)
 
         self.__unfollowed_accounts = {}
-        self.initializeUnfollowedAccountsDict(accounts)
-        self.unfollowedAccounts(database, follower_map)
+        self.__initializeUnfollowedAccountsDict(accounts)
+        self.__unfollowedAccounts(saved_follower_map, follower_map)
 
     def getFollowedAccounts(self):
         return self.__followed_accounts
@@ -24,27 +22,29 @@ class ChangeInFollowers():
         return self.__unfollowed_accounts
 
     @staticmethod
-    def getDifference(followers1: list, followers2: list) -> list:
+    def __getDifference(followers1: list, followers2: list) -> list:
         return list(set(followers1) - set(followers2))
 
-    def followedAccounts(self, database: Database, follower_map: dict) -> None:
+    def __followedAccounts(self, saved_follower_map: dict, follower_map: dict) -> None:
         accounts = follower_map.keys()
-        for account in accounts:
-            if database.containsAccount(account):
-                # obtains a list of followers that are followed that were not saved
-                self.__followed_accounts[account] = self.getDifference(follower_map[account], database.getAccountFollowers(account))
 
-    def unfollowedAccounts(self, database: Database, follower_map: dict) -> None:
-        accounts = follower_map.keys()
-        for account in database.getAccounts():
+        for account in accounts:
+            if account in saved_follower_map.keys():
+                # obtains a list of followers that are followed that were not saved
+                self.__followed_accounts[account] = ChangeInFollowers.__getDifference(follower_map[account], saved_follower_map[account])
+
+    def __unfollowedAccounts(self, saved_follower_map: dict, follower_map: dict) -> None:
+        accounts = saved_follower_map.keys()
+
+        for account in accounts:
             if account in follower_map.keys():
                 # obtains a list of follwers that were saved that are no longer followed
-                self.__unfollowed_accounts[account] = self.getDifference(database.getAccountFollowers(account), follower_map[account])
+                self.__unfollowed_accounts[account] = ChangeInFollowers.__getDifference(saved_follower_map[account], follower_map[account])
 
-    def initializeFollowedAccountsDict(self, accounts: list) -> None:
+    def __initializeFollowedAccountsDict(self, accounts: list) -> None:
         for account in accounts:
             self.__followed_accounts[account] = []
 
-    def initializeUnfollowedAccountsDict(self, accounts: list) -> None:
+    def __initializeUnfollowedAccountsDict(self, accounts: list) -> None:
         for account in accounts:
             self.__unfollowed_accounts[account] = []
