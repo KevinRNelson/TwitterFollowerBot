7dcab2cb33127f26e5f1536633094328cc817313
Kevin <keronels@ucsc.edu>
2021-07-05 21:39:22 -0700

    NewFollowerTwitterBotBuilder now extends absract TwitterBotBuilder, NewFollowerTwitterBot now extends abstract TwitterBot

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,6 +1,5 @@
+from abc import ABC, abstractmethod
 import tweepy
-
-COUNT = 200
 
 class TwitterBotBuilder():
 
@@ -27,39 +26,55 @@ class TwitterBotBuilder():
         self.wait_on_rate_limit = wait_on_rate_limit
         return self
 
-    def setAccounts(self, accounts: list):
-        self.accounts = accounts
-        return self
-
+    @abstractmethod
     def build(self):
         return TwitterBot(self)
 
+class NewFollowerTwitterBotBuilder(TwitterBotBuilder):
 
-class TwitterBot():
+    def __init__(self):
+        super().__init__()
 
-    def __init__(self, builder):
+    def build(self):
+        return NewFollowerTwitterBot(self)
+
+class TwitterBot(ABC):
+
+    @abstractmethod
+    def __init__(self, builder: TwitterBotBuilder):
+        pass
+
+    @abstractmethod
+    def setUp(self):
+        pass
+
+    @abstractmethod
+    def run(self):
+        pass
+
+class NewFollowerTwitterBot(TwitterBot):
+
+    def __init__(self, builder: NewFollowerTwitterBotBuilder):
         self.__consumer_key = builder.consumer_key
         self.__consumer_key_secret = builder.consumer_key_secret
         self.__access_token = builder.access_token
         self.__access_token_secret = builder.access_token_secret
-        self.wait_on_rate_limit = builder.wait_on_rate_limit
-        self.accounts = builder.accounts
+        self.__wait_on_rate_limit = builder.wait_on_rate_limit
 
+        self.__count = 200
+        self.__accounts = []
         self.__follower_map = {}
 
     def getFollowerMap(self):
         return self.__follower_map
 
-    def setup(self):
-        self.createApi()
+    def setUp(self, accounts: list):
+        self.__accounts = accounts
 
-    def createApi(self):
-        self.api = tweepy.API(self.authentication(), wait_on_rate_limit=self.wait_on_rate_limit)
-
-    def authentication(self):
         auth = tweepy.OAuthHandler(self.__consumer_key, self.__consumer_key_secret)
         auth.set_access_token(self.__access_token, self.__access_token_secret)
-        return auth
+
+        self.api = tweepy.API(auth, wait_on_rate_limit=self.__wait_on_rate_limit)
 
     def run(self):
         try:
@@ -68,7 +83,7 @@ class TwitterBot():
             raise e
 
     def getCurrentFollowers(self):
-        for account in self.accounts:
+        for account in self.__accounts:
             self.__follower_map[account] = []
-            for follower in tweepy.Cursor(self.api.friends, account, count=COUNT).items():
+            for follower in tweepy.Cursor(self.api.friends, account, count=self.__count).items():
                 self.__follower_map[account].append(follower.screen_name)
