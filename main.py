4d4a7559ee9bcd8c7c8fa652f1ad17ca766be971
Kevin <keronels@ucsc.edu>
2021-07-06 11:26:03 -0700

    main no longer knows about the specifics of TwitterBot. Now it only knows about the config file name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from twitter_bot import NewFollowerTwitterBotBuilder
 
 from database import PickleDatabase
 from notification import *
-from user_file_reader import UserFileReader
+from file_reader import UserFileReader, ConfigFileReader
 
 EMAIL = 'keronels@ucsc.edu'
 # FILE = 'users_test.txt'
@@ -12,27 +12,14 @@ EMAIL = 'keronels@ucsc.edu'
 FILE = 'users.txt'
 
 DATABASE = 'data.pkl'
-
-CONSUMER_KEY        = 'eINzArE3PNaD0N7jHg21RvXsz'
-CONSUMER_KEY_SECRET = 'cidYcrWYyz75sLkbrABsV9N2r2GhxZasNpWiDDh1jdPhulj8y6'
-
-ACCESS_TOKEN        = '1368620185716809728-gCid7YMfvXlXYfH0niNpqmYPG22ats'
-ACCESS_TOKEN_SECRET = 'xvQNBBnUa7VZlxBgQ44cbjt3eay8iIwwZ2UjihF92Bu62'
+CONFIG_FILE = 'config.txt'
 
 if __name__ == '__main__':
-    bot = NewFollowerTwitterBotBuilder()                                       \
-        .consumerKey(CONSUMER_KEY)                                             \
-        .consumerKeySecret(CONSUMER_KEY_SECRET)                                \
-        .accessToken(ACCESS_TOKEN)                                             \
-        .accessTokenSecret(ACCESS_TOKEN_SECRET)                                \
-        .waitOnRateLimit(True)                                                 \
-        .build()
-
     application = NewFollowerApplicationBuilder()                              \
+        .config_file(CONFIG_FILE)                                              \
         .notificationObject(TimedNotification(Print(EMAIL)))                   \
         .databaseObject(PickleDatabase(DATABASE))                              \
         .fileReaderObject(UserFileReader(FILE))                                \
-        .twitterBot(bot)                                                       \
         .build()
 
     application.setUp()
