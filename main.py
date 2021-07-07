35e8b93aa89722eface9a02d7ca48d81051eb5b1
Kevin <keronels@ucsc.edu>
2021-07-07 09:45:25 -0700

    changed Notification to CompositeNotification to emailsto multiple addresses

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from twitter_application import NewFollowerApplicationBuilder
-from notification import TimedNotification, Email, Print
+from notification import CompositeNotification, TimedNotification, EmailNotification, PrintNotification
 
-EMAIL = 'keronels@ucsc.edu'
+KERONELS = 'keronels@ucsc.edu'
 #USER_FILE = 'users_test.txt'
 
-#EMAIL = 'luckyguy1@gmail.com'
+LUCKYGUY1 = 'luckyguy1@gmail.com'
 USER_FILE = 'users.txt'
 
 DATABASE = 'data.pkl'
 CONFIG_FILE = 'config.txt'
 
 if __name__ == '__main__':
+    composite_notification = CompositeNotification()
+    composite_notification.add(EmailNotification(KERONELS))
+    composite_notification.add(EmailNotification(LUCKYGUY1))
+    timed_notification = TimedNotification(composite_notification)
+
     application = NewFollowerApplicationBuilder()                              \
         .configFile(CONFIG_FILE)                                               \
         .userFile(USER_FILE)                                                   \
-        .notificationObject(TimedNotification(Print(EMAIL)))                   \
+        .notificationObject(timed_notification)                                \
         .databaseFile(DATABASE)                                                \
         .build()
 
